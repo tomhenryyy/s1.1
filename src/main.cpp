f83b28fd2deef9e3cf64021df2d29aa4dfc97f79
thomashenry <thomashenry>
2018-01-25 09:58:17 +0000

    My first project

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,21 @@
 int main() {
 
     DigitalOut red(PTB22);
+    DigitalOut green(PTE26);
+    DigitalOut blue (PTB21);
 
     while(1) {
       red.write(0);
       wait(0.5);
       red.write(1);
+      wait(0.5);
+      green.write(0);
+      wait(0.5);
+      green.write(1);
+      wait(0.5);
+      blue.write(0);
+      wait(0.5);
+      blue.write(1);
       wait(0.5);
     }
 }
